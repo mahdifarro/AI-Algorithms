4a5f1db0c22ce1aee0f0f07758a81a0d2fc4e92a
Mahdi Farro <mahdifarro@gmail.com>
2022-05-17 22:54:41 +0430

    minor changes

--- Linear Regression/main_multipleProperties.py
+++ Linear Regression/main_multipleProperties.py
@@ -7,11 +7,11 @@ from DataVisualizer import DataVisualizer
 
 from NormalEquation import NormalEquation
 
-minXArray,maxXArray,minY,maxY=[-17,0],[5,20],7,10
-sampleCount=5
+minXArray,maxXArray,minY,maxY=[5,0],[10,15],7,10
+sampleCount=20
 
 dataSetHandler=DataSetHandler()
-dataSetHandler.GenerateContinuousDataSet_multipleProperties("Linear Regression/ContinuousDataSet_multipleProperties 1.txt",minXArray,maxXArray,minY,maxY,sampleCount)
+# dataSetHandler.GenerateContinuousDataSet_multipleProperties("Linear Regression/ContinuousDataSet_multipleProperties 1.txt",minXArray,maxXArray,minY,maxY,sampleCount)
 dataSet=dataSetHandler.ReadContinuousDataSet_multipleProperties("Linear Regression/ContinuousDataSet_multipleProperties 1.txt")
 bias=1
 weightArray=[bias,1,1]
@@ -25,10 +25,14 @@ jHistory,weightArray=lr.GradientDescent_ContinuousMultiDimensions(dataSet,weight
 J=lr.CostFunction_ContinuousMultiDemensions(dataSet,weightArray)
 #print(J)
 print(weightArray)
-
+j=lr.CostFunction_ContinuousMultiDemensions(dataSet,weightArray)
+print(j)
 ne=NormalEquation()
 NE_weights=ne.NormalEquation_multiDimensions(dataSet)
+print("NE")
 print(NE_weights)
+j=lr.CostFunction_ContinuousMultiDemensions(dataSet,NE_weights)
+print(j)
 
 dataVisualizer=DataVisualizer()
 dataVisualizer.ShowJHistory(jHistory)
--- Linear Regression/main_oneProperty.py
+++ Linear Regression/main_oneProperty.py
@@ -2,8 +2,8 @@ from DataSetHandler import DataSetHandler
 from LinearRegression import LinearRegression
 from DataVisualizer import DataVisualizer
 
-minX,maxX,minY,maxY=70,100,-50,-10
-sampleCount=1000
+minX,maxX,minY,maxY=70,100,10,60
+sampleCount=40
 
 #region Continuous DataSet
 
@@ -35,8 +35,8 @@ sampleCount=1000
 #region Discrete DataSet
 
 dataSetHandler=DataSetHandler()
-dataSetHandler.GenerateDiscreteDataSet_oneProperty("DiscreteDataSet_oneProperty 1.txt",minX,maxX,minY,maxY,sampleCount)
-dataSet_2d=dataSetHandler.ReadDiscreteDataSet_oneProperty("DiscreteDataSet_oneProperty 1.txt")
+# dataSetHandler.GenerateDiscreteDataSet_oneProperty("Linear Regression/DiscreteDataSet_oneProperty 1.txt",minX,maxX,minY,maxY,sampleCount)
+dataSet_2d=dataSetHandler.ReadDiscreteDataSet_oneProperty("Linear Regression/DiscreteDataSet_oneProperty 1.txt")
 dataSet_2d=dataSetHandler.DataSetToArrayList(dataSet_2d)
 
 slope=1
@@ -44,15 +44,17 @@ yIntercept=0
 weightArray=[slope,yIntercept]
 
 dataVisualizer=DataVisualizer()
-dataVisualizer.ShowDataSetList_2D(dataSet_2d)
-dataVisualizer.ShowLinearEquation_2D(dataSet_2d,weightArray[0],weightArray[1])
+dataVisualizer.ShowLinearEquation_Discrete2D(dataSet_2d,weightArray[0],weightArray[1])
 
 lr=LinearRegression()
-J=lr.CostFunction_2D(dataSet_2d,weightArray)
+J=lr.CostFunction_Discrete2D(dataSet_2d,weightArray)
 print(J)
-jHistory=lr.GradientDescent_2D(dataSet_2d,weightArray)
-J=lr.CostFunction_2D(dataSet_2d,weightArray)
+jHistory=lr.GradientDescent_Discrete2D(dataSet_2d,weightArray)
+J=lr.CostFunction_Discrete2D(dataSet_2d,weightArray)
 print(J)
 
-dataVisualizer.ShowLinearEquation_2D(dataSet_2d,weightArray[0],weightArray[1])
+dataVisualizer.ShowLinearEquation_Discrete2D(dataSet_2d,weightArray[0],weightArray[1])
 dataVisualizer.ShowJHistory(jHistory)
+
+
+#endregion
